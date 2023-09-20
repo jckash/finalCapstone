7df40747cc8d6e0a1593e8fc49e8f18314aa20ea
Jennifer Kash <jenniferkash3@gmail.com>
2023-09-20 13:41:23 -0400

    first

---
+++ src/main/java/com/devmountain/musicTimeCapsule/MusicTimeCapsuleApplication.java
@@ -0,0 +1,13 @@
+package com.devmountain.musicTimeCapsule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicTimeCapsuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicTimeCapsuleApplication.class, args);
+	}
+
+}
