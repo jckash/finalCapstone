e09b55b9ab804bc63981bdec3f7dc00b8cdd6897
Jennifer Kash <jenniferkash3@gmail.com>
2023-09-25 16:47:28 -0400

    Updates

--- src/main/java/com/devmountain/musicTimeCapsule/controllers/UserController.java
+++ src/main/java/com/devmountain/musicTimeCapsule/controllers/UserController.java
@@ -1,10 +1,15 @@
 package com.devmountain.musicTimeCapsule.controllers;
 
+import com.devmountain.musicTimeCapsule.dtos.UsersDto;
 import com.devmountain.musicTimeCapsule.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -15,4 +20,22 @@ public class UserController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @PostMapping("/reqister")
+    public List<String> addUser(@RequestBody UsersDto usersDto) {
+        String passHash = passwordEncoder.encode(usersDto.getPassword());
+        usersDto.setPassword(passHash);
+        return userService.addUser(usersDto);
+    }
+
+    @PostMapping("/login")
+    public List<String> userLogin(@RequestBody UsersDto usersDto){
+        return userService.userLogin(usersDto);
+
+    }
+
+
+
+
 }
+
+
