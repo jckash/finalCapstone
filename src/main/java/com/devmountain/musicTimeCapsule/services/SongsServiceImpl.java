aeef1af8bf34b0705c6290efb2a65c7ff38037f6
Jennifer Kash <jenniferkash3@gmail.com>
2023-09-25 11:44:22 -0400

    Updates

--- src/main/java/com/devmountain/musicTimeCapsule/services/SongsServiceImpl.java
+++ src/main/java/com/devmountain/musicTimeCapsule/services/SongsServiceImpl.java
@@ -1,8 +1,14 @@
 package com.devmountain.musicTimeCapsule.services;
 
+import com.devmountain.musicTimeCapsule.dtos.SongsDto;
+import com.devmountain.musicTimeCapsule.entities.Songs;
+import com.devmountain.musicTimeCapsule.entities.Users;
 import com.devmountain.musicTimeCapsule.repositories.SongsRepository;
 import com.devmountain.musicTimeCapsule.repositories.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 public class SongsServiceImpl {
 
@@ -11,4 +17,15 @@ public class SongsServiceImpl {
 
     @Autowired
     private SongsRepository songsRepository;
+
+
+public void addSong(SongsDto songsDto, Long userId){
+    Optional<Users> usersOptional = userRepository.findById(userId);
+    Songs songs = new Songs(songsDto);
+    usersOptional.ifPresent(songs::setUser);
+    songsRepository.saveAndFlush(songs);
+
+}
+
+
 }
